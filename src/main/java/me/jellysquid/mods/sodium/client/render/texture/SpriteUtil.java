35ba37af139ba479fc9fe925071b09001314bc2f
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2021-06-05 04:42:13 -0600

    change: Remove unnecessary Sprite Accessor

--- src/main/java/me/jellysquid/mods/sodium/client/render/texture/SpriteUtil.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/texture/SpriteUtil.java
@@ -1,12 +1,11 @@
 package me.jellysquid.mods.sodium.client.render.texture;
 
-import me.jellysquid.mods.sodium.mixin.features.texture_tracking.MixinSprite;
 import net.minecraft.client.texture.Sprite;
 
 public class SpriteUtil {
     public static void markSpriteActive(Sprite sprite) {
-        if (((MixinSprite) sprite).getAnimation() instanceof SpriteExtended) {
-            ((SpriteExtended) ((MixinSprite) sprite).getAnimation()).markActive();
+        if (sprite.getAnimation() instanceof SpriteExtended spriteExtended) {
+            spriteExtended.markActive();
         }
     }
 }
